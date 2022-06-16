9350a42ccad019e64e30cf69439500fc3c7b25ed
Jim Newton <jimka.issy@gmail.com>
2022-06-16 02:51:22 +0000

    white space

--- src/main/scala/convergence/Recursion.scala
+++ src/main/scala/convergence/Recursion.scala
@@ -17,23 +17,22 @@ object Recursion {
       items.head * productListOfDouble(items.tail)
   }
 
-  def sumIntBetween(lower:Int,upper:Int):Int = {
+  def sumIntBetween(lower: Int, upper: Int): Int = {
     if (lower == upper)
       upper
-    else 
-      lower + sumIntBetween(lower+1, upper)
+    else
+      lower + sumIntBetween(lower + 1, upper)
   }
 
-  def sumIntBetween2(lower:Int,upper:Int):Int = {
-    def recur(left:Int, sumSoFar:Int):Int = {
+  def sumIntBetween2(lower: Int, upper: Int): Int = {
+    def recur(left: Int, sumSoFar: Int): Int = {
       if (left == upper)
         left + sumSoFar
-      else 
-        recur(left+1, sumSoFar+left)
+      else
+        recur(left + 1, sumSoFar + left)
     }
-    recur(lower,0)
+    recur(lower, 0)
   }
-
 
   def removeFromList[T](items: List[T], deleteMe: T): List[T] = {
     ???
@@ -42,16 +41,20 @@ object Recursion {
   def removeOnce[T](items: List[T], deleteMe: T): List[T] = {
     if (items == List())
       items
-    else if ( items.head == deleteMe)
+    else if (items.head == deleteMe)
       items.tail
     else
       items.head :: removeOnce(items.tail, deleteMe)
   }
 
-  def removeOnce2[T](items: List[T], deleteMe: T, done:List[T]=List[T]()): List[T] = {
+  def removeOnce2[T](
+      items: List[T],
+      deleteMe: T,
+      done: List[T] = List[T]()
+  ): List[T] = {
     if (items == List())
       done
-    else if ( items.head == deleteMe)
+    else if (items.head == deleteMe)
       items.tail ++ done
     else
       removeOnce2(items.tail, deleteMe, items.head :: done)
@@ -75,11 +78,11 @@ object Recursion {
 
   def main(argv: Array[String]): Unit = {
 
-    //println(sumListOfInt(List(1, 3, 2, 5, 4, 3, 2, 3, -4)))
-  //println(productListOfDouble(List(1.1,2.2,3.3,-5.3)))
+    // println(sumListOfInt(List(1, 3, 2, 5, 4, 3, 2, 3, -4)))
+    // println(productListOfDouble(List(1.1,2.2,3.3,-5.3)))
     println(removeOnce2(students, "Gertrude"))
-  //println(sumIntBetween(1,10))
-  //println(sumIntBetween(10,10))
-  //println(sumIntBetween2(10,1000000))
+    // println(sumIntBetween(1,10))
+    // println(sumIntBetween(10,10))
+    // println(sumIntBetween2(10,1000000))
   }
 }
