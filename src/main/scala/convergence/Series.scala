353447f3a249e864e0bbe3fb41f0acc410c84666
Jim Newton <jimka.issy@gmail.com>
2022-06-16 02:55:13 +0000

    simplify sin function

--- src/main/scala/convergence/Series.scala
+++ src/main/scala/convergence/Series.scala
@@ -20,17 +20,8 @@ object Series {
   }
 
   def sin(x: Double, epsilon: Double = 0.0001): Double = {
-    // sum up (-1)^k * x^(2*k+1) / (2*k+1)!  for k = 0 ... inf
-
-    def recur(k: Int, sum: Double, term: Double): Double = {
-      val term2 = term * -1 * x * x / ((2 * k + 1) * (2 * k))
-      if (abs(term2) < epsilon)
-        sum + term2
-      else
-        recur(k + 1, sum + term2, term2)
-    }
-
-    recur(1, x, x)
+    import scala.math.Pi
+    cos(x - Pi / 2)
   }
 
   def cos(x: Double, epsilon: Double = 0.0001): Double = {
@@ -49,9 +40,10 @@ object Series {
   def main(argv: Array[String]): Unit = {
     println("exp = " + exp(1.0))
     println("exp = " + exp(1.0, 0.000001))
-
+    println("sin = " + sin(0.0))
     println("sin = " + sin(3.14159 / 4))
     println("sin = " + sin(3.14159 / 2))
+    println("cos = " + cos(0.0))
     println("cos = " + cos(3.14159 / 4))
     println("cos = " + cos(3.14159 / 2))
   }
