3a114a1f19418d18ceab1133a6bb89c6cd195e51
ShiningArgentum <to3707@stpatrickhighschool.net>
2022-06-17 13:31:54 +0000

    extra SPICY

--- src/main/scala/convergence/extraSpicySufferring.scala
+++ src/main/scala/convergence/extraSpicySufferring.scala
@@ -4,7 +4,7 @@ import scala.math.log
 import scala.math.abs
 object extraSpicySufferring{
     
-    def myfun(epsilon: Double):Int = {
+    /*def myfun(epsilon: Double):Int = {
         
         def powerInt(b: Int, n: Int):Int = {
             if (n % 2 == 0)
@@ -32,6 +32,32 @@ object extraSpicySufferring{
 
     def main(argv: Array[String]): Unit = {
         println(myfun(0.001))
+    }*/
+
+    def myfun(epsilon: Double):Int = {
+        
+        def recure(sumSoFar: Double, n: Int): Int = {
+
+            def power(b: Int, n: Int): Double = {
+                if (n % 2 == 0)
+                    -1
+                else
+                    1
+            }
+
+            var maybe = power(-1, n) / n
+            if (abs(((maybe + sumSoFar) - log(2))) < epsilon)
+                n
+            else
+                recure(sumSoFar + maybe, n + 1)
+
+        }
+
+        recure(0, 1)
+    }
+
+    def main(argv: Array[String]): Unit = {
+        println(myfun(0.1))
     }
 
 }
